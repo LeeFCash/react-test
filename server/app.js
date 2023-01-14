0d7efc726b186bb9f697d786c45cc944493ecfd7
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-01-14 19:29:39 +0000

    backend

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app  = express()
+
+app.get("/api", (req, res) => {
+    res.json({"user": ["userOne", "userTwo", "userThree"] })
+})
+app.listen(5000, () => { console.log("started server.") })
