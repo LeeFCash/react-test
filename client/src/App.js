e44e31400550a9a4acd7be64b019912050dac935
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-02-04 21:18:25 +0000

    start over to learn better

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,23 @@
-import react, { useEffect, useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-const [backendData, setBackendData] = useState([{}])
-
-useEffect(() => {
-  fetch("/api").then(
-    response => response.json()
-  ).then(
-    data => {
-      setBackendData(data)
-    }
-  )
-}, [])
-
-
   return (
-    <div>
-      {[typeof backendData.user === 'undefined'] ? [
-        <p>Loading -__-</p>
-       ] : (
-        backendData.users.map((user, i) => (
-         <p key={i}>{user}</p>
-        ))
-      )}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
