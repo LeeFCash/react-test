39f35cac908433f0b1d7fadfaca808004556bca2
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-02-06 22:48:13 +0000

    learn how to have more then one page kinda and what goes to what

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const name = "null";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        hello this is a test
+      </p>
+      {name ? (
+        <>
+        no
+        </>
+      ) : (
+        <h1>test</h1>
+      )}
     </div>
   );
 }
