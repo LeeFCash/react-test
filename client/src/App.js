24f4fe0f7e90a45a7fbc1e2834e5fd2d2f60f11a
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-02-07 21:57:57 +0000

    p

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 
+const gu = () => {
+  return (
+    <>
+    <p>hello</p>
+    <p>yes</p>
+    </>
+  );
+}
+
 const App = () => {
   const name = "null";
   return (
     <div className="App">
-      <p>
-        hello this is a test
-      </p>
-      {name ? (
-        <>
-        no
-        </>
-      ) : (
-        <h1>test</h1>
-      )}
+      <gu />
     </div>
   );
 }
